a37226d1d84ed4bd4da502059f917751d38ce959
PavelMinsk <pvlmby@gmail.com>
2023-04-16 19:35:01 +0300

    Shopping list added

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
-  selector: 'app-shopping-list',
-  templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+    selector: 'app-shopping-list',
+    templateUrl: './shopping-list.component.html',
+    styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent {
-
+    ingredients: Ingredient[] = [
+        new Ingredient('Tomato', 2),
+        new Ingredient('Cucumber', 1),
+        new Ingredient('Pepper', 1),
+        new Ingredient('Onion', 1),
+        new Ingredient('Garlic', 1),
+    ];
 }
